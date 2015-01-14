b475a964d3b6799a1b902a4db07062087807b1dd
dtkaplan <dtkaplan@gmail.com>
2015-01-14 11:08:40 -0600

    Updated includeSourceDocuments() to find source document name automatically.

--- R/includeSourceDocuments.R
+++ R/includeSourceDocuments.R
@@ -3,7 +3,9 @@
 #' This function, when invoked in an Rmd document, will produce links to embedded
 #' base64 encodings of source documents.  This allows you to include source files
 #' (such as Rmd, csv, etc.) inside the HTML file, so that only the HTML file need 
-#' be uploaded to sites such as RPubs.com or a course management system.
+#' be uploaded to sites such as RPubs.com or a course management system. By default, 
+#' the .Rmd document which is being compiled will be included.  You can also list files 
+#' by name.
 #' 
 #' @details Use this function in an R chunk in an Rmd file.  The chunk should 
 #' use \code{results="asis"}.
@@ -12,22 +14,21 @@
 #' 
 #' @return A character string of HTML markup containing the set of links and the
 #' base64 encoded embedded files.  The string may be quite long.
-#' 
-#' @param \code{fname} character string path and file name of a file to embed.
+#'  
+#' @param \code{...} Character strings with path and file name of a file to embed.
 #' The path should be relative to where the compilation of the Rmd file occurs, 
-#' which typically will be in the same directory as the Rmd file.
-#' @param \code{...} additional character strings naming files.
+#' which typically will be in the same directory as the Rmd file.additional character strings naming files.
 #' 
 #' @examples
 #' \dontrun{
 #' # in an R chunk in the Rmd file
-#' includeSourceDocuments("sourceFile.Rmd")
+#' includeSourceDocuments()
 #' includeSourceDocuments("sourceFile.Rmd","myData.csv","mySpreadsheet.xlsx")
 #' }
 #' @export
 
 
-includeSourceDocuments <- function(fname,...) { 
+includeSourceDocuments <- function(...) { 
   userName <- Sys.getenv("USERNAME")
   userHome <- Sys.getenv("HOME")
   toolTip <- paste('User',userName,'at',userHome)
@@ -39,17 +40,18 @@ includeSourceDocuments <- function(fname,...) {
     linkHTML <- paste0("<a href='%s' target='_blank' title='",
                       toolTip,"' download='",
                       basename(file),"'> &#8658; ",file,"</a>  ")
-    cat(sprintf(linkHTML, encodedFile))
+    sprintf(linkHTML, encodedFile)
   }
   # Do the work.
-  files <- unlist(list(fname,...)) # change arguments to a vector of strings
+  files <- unlist(list(...)) # change arguments to a vector of strings
+  # If no arguments, get the source document itself from knitr::current_input()
+  if (length(files) == 0 ) files <- knitr::current_input()
   for(F in files) { 
     if (!file.exists(F)) stop( 
       paste("File", F, 
             "doesn't exist.",
             "Are you sure you used the right name?")
     )
-    attachFile(F)
-    cat(" ")
+    cat(attachFile(F))
   }
 }
